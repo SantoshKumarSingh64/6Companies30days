d1ff0fd26f35ad980d862261f6678396a29ff623
Santosh Kumar Singh <ss7257711@gmail.com>
2021-12-31 18:12:20 +0530

    CPP Solution

---
+++ Goldman Sachs/Print Anagrams Together/Solution.cpp
@@ -0,0 +1,54 @@
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        map<string,vector<string> > m;
+        for(int i=0;i<string_list.size();i++) {
+            string str = string_list[i];
+            sort(str.begin(),str.end());
+            m[str].push_back(string_list[i]);
+        }
+        vector<vector<string> > ans;
+        for(auto &item : m) {
+            ans.push_back(item.second);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
