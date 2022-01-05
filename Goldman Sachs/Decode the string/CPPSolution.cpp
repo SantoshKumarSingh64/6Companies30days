4182acb5432e187528d8ee5f5c4df3ef99d793a0
Santosh Kumar Singh <ss7257711@gmail.com>
2022-01-05 23:03:56 +0530

    CPP Solution

---
+++ Goldman Sachs/Decode the string/CPPSolution.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string decodedString(string &s) {
+        int l = 0, r = s.size() - 1;
+        return decodeUtil(s, l, r);
+    }
+    
+    string decodeUtil(string &s, int l, int r) {
+        int times = 0;
+        string ans = "";
+        stack<char> st;
+        for(int i = l; i <= r; i++) {
+            if(isdigit(s[i])) times = times * 10 +  (s[i] - '0');
+            else if(s[i] == '[') {
+                if(times == 0) times = 1;
+                int j = i + 1;
+                while(s[j] != ']' || !st.empty()) {
+                    if(s[j] == ']') st.pop();
+                    else if(s[j] == '[') st.push('[');
+                    j++;
+                }
+                string rec = decodeUtil(s, i + 1, j - 1);
+                i = j;
+                while(times != 0) {
+                    ans = ans + rec;
+                    times--;
+                }
+            } else ans = ans + s[i];
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        Solution ob;
+        cout<<ob.decodedString(s)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
