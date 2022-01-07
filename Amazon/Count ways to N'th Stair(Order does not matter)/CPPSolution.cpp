be21c3c9acb9f471a7f4f88ed4aed5631a05d1e5
Santosh Kumar Singh <ss7257711@gmail.com>
2022-01-07 19:05:46 +0530

    C++ Solution

---
+++ Amazon/Count ways to N'th Stair(Order does not matter)/CPPSolution.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    
+    
+    long long countWays(int m)
+    {
+        // your code here
+        return 1+(m/2);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
