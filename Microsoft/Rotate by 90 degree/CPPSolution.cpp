13f2273d3432f52700516e946699c55a1e9b72f1
Santosh Kumar Singh <ss7257711@gmail.com>
2022-01-15 16:04:49 +0530

    C++ Solution

---
+++ Microsoft/Rotate by 90 degree/CPPSolution.cpp
@@ -0,0 +1,63 @@
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    int n = matrix.size();
+    
+    for(int i=0;i<n;i++){
+        int left = 0;
+        int right = n-1;
+        while(left < right){
+            swap(matrix[i][left],matrix[i][right]);
+            left++;
+            right--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        vector<vector<int> > matrix(n);
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
