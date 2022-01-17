acf6e2366adcb06c0e08371b7e30c263022874e8
Santosh Kumar Singh <ss7257711@gmail.com>
2022-01-17 11:53:41 +0530

    C++ Solution

---
+++ Adobe/Leaders in an Array/CPPSolution.cpp
@@ -0,0 +1,61 @@
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        stack<int>st;
+        st.push(a[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(st.top() <= a[i]){
+                st.push(a[i]);
+            }
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
